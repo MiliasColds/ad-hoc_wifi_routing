77394b5a30e88bd3d93c0112c7d74a61d5b0a118
Dan Bennett <bennetdj@rose-hulman.edu>
2011-04-04 22:18:54 -0400

    noasio initial commit

---
+++ SRC/basic_sender_no_asio.c
@@ -0,0 +1,64 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include "classdefs.h"
+
+
+int main(int argc, char* argv[]){
+	if (argc < 3){
+		std::cerr << "Usage: basic_sender <route_dest> <route_next> <port>\n";
+		return 1;
+  }
+  
+  struct sockaddr_in local_address, remote_address;
+  
+	char string_route_destination[16];
+	strncpy(string_route_destination, argv[1], 16);
+	char string_route_next[16];
+	strncpy(string_route_next, argv[2], 16);
+
+	int port = atoi(argv[3]);
+	char buffer[256];
+	bzero(buffer,256);
+	
+	//take in commandline arguments for easy(ier) reading
+	address static_route_destination = address( string_route_destination );
+	address static_route_next = address( string_route_next );
+	
+	//initialize the static route table
+	RouteTable table = RouteTable();
+	table.addEntry(static_route_destination, static_route_next);
+	
+	//make the packet
+	char data[30] = "the good the bad and the ugly";
+	size_t request_length = strlen(data);
+	
+	packet p = packet(
+		DAT,
+		static_route_destination,
+		30,
+		data,
+		0);
+	
+	// allocate socket
+	int sout = socket(AF_INET, SOCK_STREAM, 0);
+	if (sout < 0) error("Socket Error\n");
+	
+	// bind socket to UDP port
+	remote_address.in_family = AF_INET;
+	remote_address.in_port = htons(portno);
+	unsigned int remote_length = sizeof(struct sockaddr_in);
+	
+	int n = send_to(sout,packet,sizeof(packet),0,
+					(struct sockaddr *)&remote_address, &remote_length);
+	if (n < 0) error("Sendto Error\n");
+	
+	close(sout);
+}
