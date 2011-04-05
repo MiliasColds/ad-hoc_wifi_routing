f3eade49bef80143b51f1295ddc506ffd01a3031
MiliasColds <thorshammer01889@gmail.com>
2011-04-04 22:40:16 -0400

    finished no asio sender code methinks

--- SRC/basic_sender_no_asio.c
+++ SRC/basic_sender_no_asio.c
@@ -17,8 +17,8 @@ int main(int argc, char* argv[]){
 		return 1;
   }
   
-  struct sockaddr_in local_address, remote_address;
-  
+	struct sockaddr_in local_address, remote_address;
+	struct hostent *server;
 	char string_route_destination[16];
 	strncpy(string_route_destination, argv[1], 16);
 	char string_route_next[16];
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]){
 	//initialize the static route table
 	RouteTable table = RouteTable();
 	table.addEntry(static_route_destination, static_route_next);
-	
+	server = gethostbyname(string_route_next);
 	//make the packet
 	char data[30] = "the good the bad and the ugly";
 	size_t request_length = strlen(data);
@@ -54,10 +54,15 @@ int main(int argc, char* argv[]){
 	// bind socket to UDP port
 	remote_address.in_family = AF_INET;
 	remote_address.in_port = htons(portno);
+	bcopy((char *)server->h_addr, 
+         (char *)&remote_address.sin_addr.s_addr,
+         server->h_length);
+	 if (connect(sout,(struct sockaddr_in *) &remote_address,sizeof(remote_address)) < 0) 
+		error("ERROR connecting");
 	unsigned int remote_length = sizeof(struct sockaddr_in);
 	
-	int n = send_to(sout,packet,sizeof(packet),0,
-					(struct sockaddr *)&remote_address, &remote_length);
+	int n = write(sout,packet,sizeof(packet));//,0,
+					//(struct sockaddr *)&remote_address, &remote_length);
 	if (n < 0) error("Sendto Error\n");
 	
 	close(sout);
